5a015869f3210158e75914c4555ec14c6c970c0e
Roo <roo@antigraviti.com>
2026-03-30 21:36:44 +0700

    fix(etl): move memoryStatusEx to local scope to fix IDE redeclaration error

--- neuradash-backend/internal/utils/mem_windows.go
+++ neuradash-backend/internal/utils/mem_windows.go
@@ -7,20 +7,20 @@ import (
 	"unsafe"
 )
 
-type memoryStatusEx struct {
-	dwLength                uint32
-	dwMemoryLoad            uint32
-	ullTotalPhys            uint64
-	ullAvailPhys            uint64
-	ullTotalPageFile        uint64
-	ullAvailPageFile        uint64
-	ullTotalVirtual         uint64
-	ullAvailVirtual         uint64
-	ullAvailExtendedVirtual uint64
-}
-
 // GetSystemAvailableMemory returns the available physical memory in MB on Windows.
 func GetSystemAvailableMemory() uint64 {
+	type memoryStatusEx struct {
+		dwLength                uint32
+		dwMemoryLoad            uint32
+		ullTotalPhys            uint64
+		ullAvailPhys            uint64
+		ullTotalPageFile        uint64
+		ullAvailPageFile        uint64
+		ullTotalVirtual         uint64
+		ullAvailVirtual         uint64
+		ullAvailExtendedVirtual uint64
+	}
+
 	kernel32 := syscall.NewLazyDLL("kernel32.dll")
 	globalMemoryStatusEx := kernel32.NewProc("GlobalMemoryStatusEx")
 	var ms memoryStatusEx
